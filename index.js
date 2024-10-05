ae504a90fed4b8c442b19f22dbc653ec6e5dbdb9
+ <kibetg984@gmail.com>
2024-10-05 12:35:59 +0300

    Worked on JavaScript Event Listeners

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button'); // Select the button element
+    input.addEventListener('click', function() {     // Add click event listener
+      alert('I was clicked!');
+    });
+  }
+  
+  // Call the function to bind the event listener
+  addingEventListener();
+  
